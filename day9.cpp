cce133cb57a6f99f34a64604b873b8bb32a6fbed
RaduXD1 <58386255+RaduXD1@users.noreply.github.com>
2019-12-09 08:59:38 +0200

    day9

---
+++ day9.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+ifstream fin("date.in");
+ofstream fout("date.out");
+long long n,b,i,sol,f[100],a[10000],v1,v2,ok,aux,nr,poz,base;
+int main()
+{
+    while(fin>>b) a[++n]=b;
+    base=1;
+    for(i=1;i<=n;i++)
+    {
+        aux=a[i];nr=0;
+        for(ok=1;ok<=7;ok++) f[ok]=0;
+        while(aux) f[++nr]=aux%10,aux/=10;
+        if(f[1]==9&&f[2]==9) break;
+        if(f[1]==3)
+        {
+            if(f[3]==0) poz=a[++i]+1;
+            else if(f[3]==1) poz=++i;
+            else if(f[3]==2) poz=a[++i]+base;
+            a[poz]=2;
+        }
+        else if(f[1]==4)
+        {
+            if(f[3]==0) poz=a[++i]+1;
+            else if(f[3]==1) poz=++i;
+            else if(f[3]==2) poz=a[++i]+base;
+            fout<<a[poz]<<"\n";
+        }
+        else if(f[1]==2||f[1]==1||f[1]==7||f[1]==8)
+        {
+            if(f[3]==0) v1=a[a[++i]+1];
+            else if(f[3]==1) v1=a[++i];
+            else v1=a[a[++i]+base];
+
+            if(f[4]==0) v2=a[a[++i]+1];
+            else if(f[4]==1) v2=a[++i];
+            else v2=a[a[++i]+base];
+
+            if(f[5]==0) poz=a[++i]+1;
+            else if(f[5]==1) poz=++i;
+            else if(f[5]==2) poz=a[++i]+base;
+
+            if(f[1]==1) a[poz]=v1+v2;
+            if(f[1]==2) a[poz]=v1*v2;
+            if(f[1]==7) a[poz]=(v1<v2);
+            if(f[1]==8) a[poz]=(v1==v2);
+        }
+        else if(f[1]==5||f[1]==6)
+        {
+            if(f[3]==0) v1=a[a[++i]+1];
+            else if(f[3]==1) v1=a[++i];
+            else v1=a[a[++i]+base];
+
+            if(f[4]==0) v2=a[a[++i]+1];
+            else if(f[4]==1) v2=a[++i];
+            else v2=a[a[++i]+base];
+
+            if(f[1]==5&&v1!=0) i=v2;
+            else if(f[1]==6&&v1==0) i=v2;
+        }
+        else if(f[1]==9)
+        {
+            if(f[3]==0) v1=a[a[++i]+1];
+            else if(f[3]==1) v1=a[++i];
+            else v1=a[a[++i]+base];
+            base+=v1;
+        }
+    }
+    return 0;
+}
